11a99e658fa755ea49c6d5400a6df4ca1afb77ae
Blake Harper <blakerobertharper@gmail.com>
2017-04-03 16:46:37 -0400

    Disabled security for demo

--- src/main/java/com/onewheelwizard/bakery/security/WebSecurityConfiguration.java
+++ src/main/java/com/onewheelwizard/bakery/security/WebSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.onewheelwizard.bakery.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,9 +12,13 @@ class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().fullyAuthenticated().and().
-                httpBasic().and().
+        http
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.POST,"/accounts").permitAll()
+                    .anyRequest().permitAll()//fullyAuthenticated()
+                    .and().
+                //httpBasic()
+                //    .and().
                 csrf().disable();
     }
 
